5d6913c61fa85b074dc9bb1063651f1db1a80107
onatcipli <onatcipli@outlook.com>
2019-11-25 20:16:16 +0300

    deleting dart file

--- lib/views/layout_slide/whatsapp_clone_slide.dart
+++ lib/views/layout_slide/whatsapp_clone_slide.dart
@@ -1,5 +1,4 @@
 import 'package:animation_presentation/views/layout_slide/mobile_device.dart';
-import 'package:animation_presentation/views/whatsapp_ui_clone/whatsapp_clone.dart';
 import 'package:animation_presentation/widgets/slide_stack.dart/slide_stack.dart';
 import 'package:flutter/material.dart';
 
